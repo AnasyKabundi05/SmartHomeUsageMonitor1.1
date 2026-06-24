16cea42e373de2262f570d0af6301352c0e2b814
anasy <AnasyKabundi00@gmail.com>
2026-06-24 20:59:40 +0100

    Updated with CORS with origin = * in my controller due the frontend not connecting with the backend

--- src/main/java/org/example/smarthomeusagemonitor/api/UsageLogController.java
+++ src/main/java/org/example/smarthomeusagemonitor/api/UsageLogController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://35.172.27.21")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/usageLog")
 public class UsageLogController {
 
